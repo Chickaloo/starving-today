7fdd5b0f979ea9c05bcd36023f627ae55a0e7d5d
Anna Olshanskaya <olshanskaya.anna@gmail.com>
2018-02-12 23:44:12 -0800

    Took addRecipe controller out of the landingPage controller file

--- components/landingPage/landingPageController.js
+++ components/landingPage/landingPageController.js
@@ -28,45 +28,12 @@ angular.module('starvingToday').controller('landingController', ['$scope', '$htt
 		});
 	}
 }]);
-<<<<<<< HEAD
-=======
-
-var app = angular.module("add-recipe-app", []);
-app.controller("addRecipeController", function ($scope, $http) {
-	$scope.SendData = function () {
-	// use $.param jQuery function to serialize data from JSON
-		var data = {
-			authorid: parseInt($scope.authorid),
-			title: $scope.title,
-			instructions: $scope.instructions
-		};
-
-		var parameter = JSON.stringify(data);
-
-		var config = {
-			headers : {
-				'Content-Type': 'application/json;charset=utf-8'
-			}
-		}
-
-		$http.post('http://138.68.22.10:81/api/recipes', parameter, config)
-		.success(function (data, status, headers, config) {
-			$scope.PostDataResponse = data;
-		})
-		.error(function (data, status, header, config) {
-			$scope.ResponseDetails = "Data: " + data +
-				"<hr />status: " + status +
-				"<hr />headers: " + header +
-				"<hr />config: " + config;
-		});
-	};
-});
 
 angular.module('starvingToday').controller('statsController', ['$scope', '$http', function($scope, $http)
 {
     $http.get('http://138.68.22.10:84/stats')
     .then(function (response) {
         $scope.recipeCount = response.data.recipecount;
-			$scope.userCount = response.data.usercount;});
+				$scope.userCount = response.data.usercount;
+		});
 }]);
->>>>>>> 05146912f24208f88ac4ad5612d661463a0b084f
