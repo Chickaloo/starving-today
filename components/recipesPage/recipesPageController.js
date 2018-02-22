cb3525c2ab220c5a230d24c80e1ee0b4175d2fc7
kjasonk <kjasonk@gmail.com>
2018-02-22 15:51:16 -0800

    Code Rewrite
    Switched to Factory for RecipePage

--- components/recipesPage/recipesPageController.js
+++ components/recipesPage/recipesPageController.js
@@ -1,6 +1,29 @@
-angular.module('starvingToday').controller('recipesController', ['$scope', '$http', function($scope, $http) 
+angular.module('starvingToday').factory('dataRecipe', ['$http', function ($http) {
+    var dataRecipe = {};
+    
+    dataRecipe.getRecipeDump = function () {
+        return $http.get('http://138.68.22.10:84/recipes');
+    };
+    
+    dataRecipe.getRecipe = function (recipeId) {
+        return $http.get('http://138.68.22.10:84/recipes/id/' + recipeId)
+    };
+    
+    dataRecipe.searchRecipe = function () {
+        return $http.get('http://138.68.22.10:84/recipe/id' + )
+    }
+    
+    return dataRecipe;
+}]);
+
+angular.module('starvingToday').controller('recipesController', ['$scope', '$http', 'dataRecipe', function($scope, $http, dataRecipe) 
 {
-    $http.get('http://138.68.22.10:84/recipes')
-    .then(function (response) {
-        $scope.recipes = response.data.recipes;});
+    $scope.recipes;
+    
+    getRecipeDump();
+    function getRecipeDump(){
+        dataRecipe.getRecipeDump()
+            .then(function (response) {
+                $scope.recipes = response.data.recipes;});
+    }
 }]);
