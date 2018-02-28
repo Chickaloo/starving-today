56fcbddaf809ae99ab645859b842d1c8256a7a2d
Anna Olshanskaya <olshanskaya.anna@gmail.com>
2018-02-27 21:51:39 -0800

    got user infor to show on edit user popup

--- components/myHub/myHubController.js
+++ components/myHub/myHubController.js
@@ -1,5 +1,37 @@
 angular.module('starvingToday').controller('myHubController', ['$scope', '$http', 'dataUser', function($scope, $http, dataUser)
 {
     $scope.user = dataUser.user;
-    $scope.tempuser = dataUser.user;
+    $scope.fullname = dataUser.user.firstname + " " + dataUser.user.lastname;
+    $scope.bio = dataUser.user.bio + " ";
+
+    var user_data = {
+			firstname: $scope.fullname,
+			email: $scope.user.email,
+      bio: $scope.bio
+		};
+
+		var data = JSON.stringify(user_data);
+
+		var config = {
+  			headers : {
+  				'Content-Type': 'application/json;charset=UTF-8'
+  			}
+  		}
+
+		$http.put('http://138.68.22.10:84/users', data, config)
+		.then(
+			function (response) {
+        if (response.status === 200) {
+            $scope.responseDetails = "User info updated successfully.";
+        }
+      },
+      function (response) {
+        if (response.status === 500) {
+            $scope.responseDetails = "Something went wrong with our servers!";
+        } else if(response.status === 404){
+            $scope.responseDetails = "Account not properly created.";
+        } else {
+            $scope.responseDetails = "Everything is broken. Please abandon ship.";
+        }
+    });
 }]);
