92acd0bf03d2950c55dfb1930039899e9b5986d5
Chris <chris@Macaulays-MacBook-Pro.local>
2018-03-01 17:47:17 -0800

    fixed merge headers

--- components/myHub/myHubController.js
+++ components/myHub/myHubController.js
@@ -3,43 +3,6 @@ angular.module('starvingToday').controller('myHubController', ['$scope', '$http'
     $scope.user = dataUser.user;
     $scope.reciperating = 0;
 
-    //SEARCH BY THIS USER TO POPULATE THEIR RECIPIES
-<<<<<<< HEAD
-		var config = {
-      withCredentials: 'true',
-			headers : {
-				'Content-Type': 'application/json;charset=UTF-8'
-			}
-  	}
-
-		$http.get('http://138.68.22.10:84/recipes/user/'+dataUser.user.userid, config)
-		.then(
-			function (response) {
-				$scope.userrecipes = response.data.recipes;
-        $scope.recipecount = Object.keys($scope.userrecipes).length;
-        angular.forEach($scope.userrecipes, function(value, key){
-          $scope.reciperating = $scope.reciperating + value.upvotes - value.downvotes;
-        });
-			},
-			function (response) {
-				if (response.status === 500) {
-						$scope.responseDetails = "Something went wrong with our servers!";
-				} else if(response.status === 400){
-						$scope.responseDetails = "The input was invalid. Please try again.";
-				} else if(response.status === 404){
-						$scope.responseDetails = "No recipes were found.";
-				} else {
-						$scope.responseDetails = "Something broke!";
-				}
-		});
-
-    $scope.selectRecipe = function(value){
-      dataRecipe.popRecipe();
-      dataRecipe.pushRecipe(value);
-      console.log(value);
-    }
-=======
-
   		var config = {
           withCredentials: 'true',
     			headers : {
@@ -74,5 +37,4 @@ angular.module('starvingToday').controller('myHubController', ['$scope', '$http'
           console.log(dataRecipe.getCurrRecipe());
           $state.go('viewRecipesState', {}, {reload:true});
       }
->>>>>>> 015937ff7adb07baf54611a3aad111db380ea4ad
 }]);
--- components/viewRecipePage/viewRecipePageController.js
+++ components/viewRecipePage/viewRecipePageController.js
@@ -1,29 +1,5 @@
 angular.module('starvingToday').controller('viewRecipeController', ['$scope', '$http', '$state', 'dataRecipe', 'dataUser', function($scope, $http, $state, dataRecipe, dataUser)
 {
-<<<<<<< HEAD
-    $scope.recipe;
-    $scope.user = dataUser.user;
-
-    getRecipe();
-    function getRecipe() {
-        dataRecipe.getRecipe()
-            .then(
-              function (response) {
-                console.log(response.data);
-                $scope.recipe = response.data;
-              },function (response) {
-              		if (response.status === 500) {
-              				$scope.responseDetails = "Please double check your username and password!";
-              		} else if(response.status === 400){
-              				$scope.responseDetails = "Please double check your username and password!";
-              		} else if(response.status === 404){
-              				$scope.responseDetails = "Please double check your username and password!";
-              		} else {
-              				$scope.responseDetails = "Oops! Something went wrong! Please try signing in again.";
-              		}
-              	});
-    }
-=======
 
     $scope.recipe = dataRecipe.getCurrRecipe();
     $scope.user = dataUser.user;
@@ -48,5 +24,4 @@ angular.module('starvingToday').controller('viewRecipeController', ['$scope', '$
     //           	});
     // }
 
->>>>>>> 015937ff7adb07baf54611a3aad111db380ea4ad
 }]);
