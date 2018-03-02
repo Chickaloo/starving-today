2875e3cbcbf6c71f22a6ffe8b75e20c17e8f7df5
Chris <chris@Macaulays-MacBook-Pro.local>
2018-03-02 04:35:47 -0800

    bugfixes, formatting fixes, and added formatting for recipes

--- components/viewRecipePage/viewRecipePageController.js
+++ components/viewRecipePage/viewRecipePageController.js
@@ -1,9 +1,10 @@
-angular.module('starvingToday').controller('viewRecipeController', ['$scope', '$http', '$state', 'dataRecipe', 'dataUser', function($scope, $http, $state, dataRecipe, dataUser)
+angular.module('starvingToday').controller('viewRecipeController', ['$scope', '$http', '$state', '$sce', 'dataRecipe', 'dataUser', function($scope, $http, $state, $sce, dataRecipe, dataUser)
 {
 
     console.log("Loaded into view");
 
     $scope.recipe = dataRecipe.getCurrRecipe();
+    $scope.recipe.recipeinstructions = $sce.trustAsHtml($scope.recipe.recipeinstructions);
     $scope.recipelen = dataRecipe.getRecipeLength();
     $scope.user = dataUser.user;
     console.log($scope.recipe);
@@ -13,7 +14,15 @@ angular.module('starvingToday').controller('viewRecipeController', ['$scope', '$
           var temp = [];
           if (typeof response.data.comments !== "undefined") {
             Object.keys(response.data.comments).forEach(function(key) {
-                temp.push(response.data.comments[key]);
+              $http.get('http://138.68.22.10:84/users/id/' + response.data.comments[key].userid).then(
+                function (res) {
+                  console.log(res.data.user.firstname + " " + res.data.user.lastname);
+                  response.data.comments[key].authorname = res.data.user.firstname + " " + res.data.user.lastname;
+                },
+                function (res) {
+                    $scope.comments = 0;
+              });
+              temp.push(response.data.comments[key]);
             });
             $scope.comments = temp.reverse();
           }
@@ -32,12 +41,12 @@ angular.module('starvingToday').controller('viewRecipeController', ['$scope', '$
 
         var data = JSON.stringify(comment_data);
 
-		var config = {
-            withCredentials: 'true',
-  			headers : {
-  				'Content-Type': 'application/json;charset=UTF-8'
-  			}
-  		}
+  		var config = {
+              withCredentials: 'true',
+    			headers : {
+    				'Content-Type': 'application/json;charset=UTF-8'
+    			}
+    		}
 
         $http.post('http://138.68.22.10:84/comments', data, config)
         .then(function(response) {
@@ -46,6 +55,14 @@ angular.module('starvingToday').controller('viewRecipeController', ['$scope', '$
               console.log(response.data);
                 var temp = [];
                 Object.keys(response.data.comments).forEach(function(key) {
+                  $http.get('http://138.68.22.10:84/users/id/' + response.data.comments[key].userid).then(
+                    function (res) {
+                      console.log(res.data.user.firstname + " " + res.data.user.lastname);
+                      response.data.comments[key].authorname = res.data.user.firstname + " " + res.data.user.lastname;
+                    },
+                    function (res) {
+                        $scope.comments = 0;
+                  });
                     temp.push(response.data.comments[key]);
                 });
                 $scope.comments = temp.reverse();
@@ -55,4 +72,33 @@ angular.module('starvingToday').controller('viewRecipeController', ['$scope', '$
           });
         });
     }
+
+    $scope.DeleteComment = function(value) {
+
+
+      $http.delete('http://138.68.22.10:84/comments/' + value)
+      .then(function(response) {
+        $http.get('http://138.68.22.10:84/comments/recipe/' + $scope.recipe.recipeid).then(
+          function (response) {
+            console.log(response.data);
+              var temp = [];
+              Object.keys(response.data.comments).forEach(function(key) {
+                $http.get('http://138.68.22.10:84/users/id/' + response.data.comments[key].userid).then(
+                  function (res) {
+                    console.log(res.data.user.firstname + " " + res.data.user.lastname);
+                    response.data.comments[key].authorname = res.data.user.firstname + " " + res.data.user.lastname;
+                  },
+                  function (res) {
+                      $scope.comments = 0;
+                });
+                  temp.push(response.data.comments[key]);
+              });
+              $scope.comments = temp.reverse();
+          },
+          function (response) {
+              $scope.comments = 0;
+        });
+      });
+    }
+
 }]);
