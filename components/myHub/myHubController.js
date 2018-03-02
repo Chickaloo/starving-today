26a8da11dc34b14373514721440bb90982bbc879
Anna Olshanskaya <olshanskaya.anna@gmail.com>
2018-03-01 19:36:47 -0800

    -more fucking up

--- components/myHub/myHubController.js
+++ components/myHub/myHubController.js
@@ -3,32 +3,34 @@ angular.module('starvingToday').controller('myHubController', ['$scope', '$http'
     $scope.user = dataUser.user;
     $scope.reciperating = 0;
 
-  		var config = {
-          withCredentials: 'true',
-    			headers : {
-    				'Content-Type': 'application/json;charset=UTF-8'
-    			}
-    		}
+    //SEARCH BY THIS USER TO POPULATE THEIR RECIPIES
 
-  		$http.get('http://138.68.22.10:84/recipes/user/'+dataUser.user.userid, config)
-  		.then(
-  			function (response) {
-					$scope.userrecipes = response.data.recipes;
-          $scope.recipecount = Object.keys($scope.userrecipes).length;
-          angular.forEach($scope.userrecipes, function(value, key){
-            $scope.reciperating = $scope.reciperating + value.upvotes - value.downvotes;
-          });
-  			},
-  			function (response) {
-  				if (response.status === 500) {
-  						$scope.responseDetails = "Something went wrong with our servers!";
-  				} else if(response.status === 400){
-  						$scope.responseDetails = "The input was invalid. Please try again.";
-  				} else if(response.status === 404){
-  						$scope.responseDetails = "No recipes were found.";
-  				} else {
-  						$scope.responseDetails = "Something broke!";
-  				}
+		var config = {
+        withCredentials: 'true',
+  			headers : {
+  				'Content-Type': 'application/json;charset=UTF-8'
+  			}
+  		}
+
+		$http.get('http://138.68.22.10:84/recipes/user/'+dataUser.user.userid, config)
+		.then(
+			function (response) {
+				$scope.userrecipes = response.data.recipes;
+        $scope.recipecount = Object.keys($scope.userrecipes).length;
+        angular.forEach($scope.userrecipes, function(value, key){
+          $scope.reciperating = $scope.reciperating + value.upvotes - value.downvotes;
+        });
+			},
+			function (response) {
+				if (response.status === 500) {
+						$scope.responseDetails = "Something went wrong with our servers!";
+				} else if(response.status === 400){
+						$scope.responseDetails = "The input was invalid. Please try again.";
+				} else if(response.status === 404){
+						$scope.responseDetails = "No recipes were found.";
+				} else {
+						$scope.responseDetails = "Something broke!";
+				}
   		});
 
       $scope.selectRecipe = function(value){
