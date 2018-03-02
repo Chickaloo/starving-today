1b209dd24c0de38166cbf37c16fa1c61f426358e
Anna Olshanskaya <olshanskaya.anna@gmail.com>
2018-03-01 17:40:59 -0800

    -broke everything

--- components/myHub/myHubController.js
+++ components/myHub/myHubController.js
@@ -1,76 +1,40 @@
 angular.module('starvingToday').controller('myHubController', ['$scope', '$http', 'dataUser', 'dataRecipe', function($scope, $http, dataUser, dataRecipe)
 {
     $scope.user = dataUser.user;
-    $scope.fullname = dataUser.user.firstname + " " + dataUser.user.lastname;
-    $scope.bio = dataUser.user.bio + " ";
     $scope.reciperating = 0;
-    $scope.updateUser = function(){
-      var user_data = {
-  			firstname: $scope.fullname,
-  			email: $scope.user.email,
-        bio: $scope.bio
-  		};
-
-  		var data = JSON.stringify(user_data);
-
-  		var config = {
-    			headers : {
-    				'Content-Type': 'application/json;charset=UTF-8'
-    			}
-    		}
-
-  		$http.put('http://138.68.22.10:84/users', data, config)
-  		.then(
-  			function (response) {
-          if (response.status === 200) {
-              $scope.responseDetails = "User info updated successfully.";
-          }
-        },
-        function (response) {
-          if (response.status === 500) {
-              $scope.responseDetails = "Something went wrong with our servers!";
-          } else if(response.status === 404){
-              $scope.responseDetails = "Account not properly created.";
-          } else {
-              $scope.responseDetails = "Everything is broken. Please abandon ship.";
-          }
-      });
-    }
-
 
     //SEARCH BY THIS USER TO POPULATE THEIR RECIPIES
+		var config = {
+      withCredentials: 'true',
+			headers : {
+				'Content-Type': 'application/json;charset=UTF-8'
+			}
+  	}
 
-  		var config = {
-          withCredentials: 'true',
-    			headers : {
-    				'Content-Type': 'application/json;charset=UTF-8'
-    			}
-    		}
+		$http.get('http://138.68.22.10:84/recipes/user/'+dataUser.user.userid, config)
+		.then(
+			function (response) {
+				$scope.userrecipes = response.data.recipes;
+        $scope.recipecount = Object.keys($scope.userrecipes).length;
+        angular.forEach($scope.userrecipes, function(value, key){
+          $scope.reciperating = $scope.reciperating + value.upvotes - value.downvotes;
+        });
+			},
+			function (response) {
+				if (response.status === 500) {
+						$scope.responseDetails = "Something went wrong with our servers!";
+				} else if(response.status === 400){
+						$scope.responseDetails = "The input was invalid. Please try again.";
+				} else if(response.status === 404){
+						$scope.responseDetails = "No recipes were found.";
+				} else {
+						$scope.responseDetails = "Something broke!";
+				}
+		});
 
-  		$http.get('http://138.68.22.10:84/recipes/user/'+dataUser.user.userid, config)
-  		.then(
-  			function (response) {
-					$scope.userrecipes = response.data.recipes;
-          $scope.recipecount = Object.keys($scope.userrecipes).length;
-          angular.forEach($scope.userrecipes, function(value, key){
-            $scope.reciperating = $scope.reciperating + value.upvotes - value.downvotes;
-          });
-  			},
-  			function (response) {
-  				if (response.status === 500) {
-  						$scope.responseDetails = "Something went wrong with our servers!";
-  				} else if(response.status === 400){
-  						$scope.responseDetails = "The input was invalid. Please try again.";
-  				} else if(response.status === 404){
-  						$scope.responseDetails = "No recipes were found.";
-  				} else {
-  						$scope.responseDetails = "Something broke!";
-  				}
-  		});
-
-        $scope.selectRecipe = function(value){
-                dataRecipe.popRecipe();
-                dataRecipe.pushRecipe(value);
-                console.log(value);
-        }
+    $scope.selectRecipe = function(value){
+      dataRecipe.popRecipe();
+      dataRecipe.pushRecipe(value);
+      console.log(value);
+    }
 }]);
--- components/viewRecipePage/viewRecipePageController.js
+++ components/viewRecipePage/viewRecipePageController.js
@@ -22,40 +22,4 @@ angular.module('starvingToday').controller('viewRecipeController', ['$scope', '$
               		}
               	});
     }
-
-    $scope.SendData = function() {
-      if ($scope.user.userid === 0) {
-        $scope.responseDetails = "not logged in!";
-        console.log($scope.responseDetails);
-        return 1
-      }
-
-  		var recipe_data = {
-  			userid: parseInt($scope.user.userid),
-  			recipename: $scope.recipename,
-  			recipedescription: $scope.recipedescription,
-  			recipeinstructions: $scope.recipeinstructions,
-  			calories: parseInt($scope.calories),
-  			preptime: parseInt($scope.preptime),
-  			cooktime: parseInt($scope.cooktime),
-  			servings: parseInt($scope.servings)
-  		};
-
-  		var data = JSON.stringify(recipe_data);
-
-  		var config = {
-  			headers : {
-  				'Content-Type': 'application/json;charset=utf-8'
-  			}
-  		}
-
-  		$http.post('http://138.68.22.10:84/recipes', data, config)
-  		.then(
-  			function (response) {
-  				$scope.responseDetails = "You entered a recipe! Eww!";
-  			},
-  			function (response) {
-  				$scope.responseDetails = "You couldn't even enter a recipe correctly.. for SHAME!" + response.status;
-  		});
-  	}
 }]);
