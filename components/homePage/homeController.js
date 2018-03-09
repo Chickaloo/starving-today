cce4891210feb5addfd8de178cc5e7b5714e67da
kjasonk <kjasonk@gmail.com>
2018-03-09 02:30:32 -0800

    Front Page

--- components/homePage/homeController.js
+++ components/homePage/homeController.js
@@ -10,4 +10,24 @@ angular.module('starvingToday').controller('homeController', ['$scope', '$http',
       });
     $state.go('yourHub','',{reload:true});
   }
+
+
+    $http.get('http://138.68.22.10:84/recipes/id/20').then(
+      function(response){
+          $scope.Favorite = response.data;
+      }
+    )
+
+    $http.get('http://138.68.22.10:84/recipes/id/9').then(
+      function(response){
+          $scope.Highest = response.data;
+      }
+    )
+
+    $http.get('http://138.68.22.10:84/recipes/id/184').then(
+      function(response){
+          $scope.TeamFav = response.data;
+      }
+    )
+
 }]);
